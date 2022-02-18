98f1bfe6bcc77859d6b3f6dfb0516e04a85434ab
Glauco Pantoni <glaucopantoni@gmail.com>
2022-02-18 16:08:53 -0300

    Begins the project

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express, { json } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(json());
+app.use(router);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on PORT ${process.env.PORT}`)
+);
