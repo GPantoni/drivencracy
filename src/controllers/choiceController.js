4d708f61564fc03ea12f3a25a8bec487cbeeaa8d
Glauco Pantoni <glaucopantoni@gmail.com>
2022-02-20 20:28:34 -0300

    fix/#1-generic global fixes

--- src/controllers/choiceController.js
+++ src/controllers/choiceController.js
@@ -6,34 +6,37 @@ import isSameOrBefore from "dayjs/plugin/isSameOrBefore.js";
 export async function createChoice(req, res) {
   const { title, poolId } = req.body;
   const now = dayjs().format("YYYY-MM-DD HH:mm");
-  console.log(now);
+  // console.log(now);
 
-  const isAPool = await db
-    .collection("pools")
-    .findOne({ _id: new ObjectId(poolId) });
-  if (!isAPool) {
-    return res.sendStatus(404);
-  }
-
-  const usedTitle = await db.collection("choices").findOne({ title });
-  console.log(usedTitle);
-  if (usedTitle) {
-    return res.sendStatus(409);
-  }
-
-  dayjs.extend(isSameOrBefore);
-  const expireDate = await db
-    .collection("pools")
-    .findOne({ _id: new ObjectId(poolId) });
-  console.log(expireDate);
-  if (dayjs(expireDate.expireAt).isSameOrBefore(dayjs(now))) {
-    return res.sendStatus(403);
-  }
   try {
-    await db.collection("choices").insertOne({ title, poolId });
+    const isAPool = await db
+      .collection("pools")
+      .findOne({ _id: new ObjectId(poolId) });
+    if (!isAPool) {
+      return res.sendStatus(404);
+    }
+
+    const usedTitle = await db.collection("choices").findOne({ title });
+    // console.log(usedTitle);
+    if (usedTitle) {
+      return res.sendStatus(409);
+    }
+
+    dayjs.extend(isSameOrBefore);
+    const expireDate = await db
+      .collection("pools")
+      .findOne({ _id: new ObjectId(poolId) });
+    // console.log(expireDate);
+    if (dayjs(expireDate.expireAt).isSameOrBefore(dayjs(now))) {
+      return res.sendStatus(403);
+    }
+
+    await db
+      .collection("choices")
+      .insertOne({ title, poolId: new ObjectId(poolId) });
     res.sendStatus(201);
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
     res.sendStatus(500);
   }
 }
