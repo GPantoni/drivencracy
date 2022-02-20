a5e9268818ef516a9d8442e48b2dfe032b4c5b8f
Glauco Pantoni <glaucopantoni@gmail.com>
2022-02-20 16:02:43 -0300

    Add GET/pool/:id/choice functionality

--- src/controllers/poolController.js
+++ src/controllers/poolController.js
@@ -1,5 +1,6 @@
 import db from "../database.js";
 import dayjs from "dayjs";
+import { ObjectId } from "mongodb";
 
 export async function createPool(req, res) {
   let pool = req.body;
@@ -27,6 +28,28 @@ export async function listPools(req, res) {
   try {
     const pools = await db.collection("pools").find({}).toArray();
     res.send(pools);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function listPoolChoices(req, res) {
+  const { id } = req.params;
+  // console.log(id);
+
+  const pool = await db.collection("pools").findOne({ _id: new ObjectId(id) });
+  if (!pool) {
+    return res.sendStatus(404);
+  }
+
+  try {
+    const poolChoices = await db
+      .collection("choices")
+      .find({ poolId: id })
+      .toArray();
+    // console.log(poolChoices);
+    return res.send(poolChoices);
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
--- src/routes/poolRouter.js
+++ src/routes/poolRouter.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { validatePoolSchema } from "../middlewares/validatePoolSchema.js";
-import { createPool, listPools } from "../controllers/poolController.js";
+import {
+  createPool,
+  listPools,
+  listPoolChoices,
+} from "../controllers/poolController.js";
 
 const poolRouter = Router();
 
 poolRouter.post("/pool", validatePoolSchema, createPool);
 poolRouter.get("/pool", listPools);
+poolRouter.get("/pool/:id/choice", listPoolChoices);
 
 export default poolRouter;
