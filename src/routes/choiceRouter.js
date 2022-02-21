b6f54260493c63f41fe187f95fdfcc0e3a945c91
Glauco Pantoni <glaucopantoni@gmail.com>
2022-02-21 12:22:34 -0300

    Add POST/choice/:id/vote functionality

--- src/routes/choiceRouter.js
+++ src/routes/choiceRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { validateChoiceSchema } from "../middlewares/validateChoiceSchema.js";
-import { createChoice } from "../controllers/choiceController.js";
+import { createChoice, registerVote } from "../controllers/choiceController.js";
 
 const choiceRouter = Router();
 
 choiceRouter.post("/choice", validateChoiceSchema, createChoice);
+choiceRouter.post("/choice/:id/vote", registerVote);
 
 export default choiceRouter;
